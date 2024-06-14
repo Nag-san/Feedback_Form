e6bd45f5c30a75afb7f4e16d72214f875dcd0563
Nag-san <151753732+Nag-san@users.noreply.github.com>
2024-06-14 12:30:54 +0530

    Added Display feature & Ui Changes

--- Frontend/src/pages/CreateFeedBack.tsx
+++ Frontend/src/pages/CreateFeedBack.tsx
@@ -1,11 +1,16 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const CreateFeedback = () =>{
 const [name, setName] = useState("");
 const [feedback_desc, setFeedback_desc] = useState("");
 
 const saveFeedback = () => {
+    if (!name || !feedback_desc) {
+        alert("Enter both name and feedback descriptions!")
+        return
+    }
     const data = {
         "name": name,
         "feedback_desc": feedback_desc
@@ -14,6 +19,8 @@ const saveFeedback = () => {
     .post('http://localhost:5000/feedback',data)
     .then(()=>{
         console.log(data);
+        alert("Feedback succesfull!");
+
     })
     .catch((error)=>{
         console.log(error);
@@ -23,20 +30,33 @@ const saveFeedback = () => {
 
 return (
 <div className="p-4">
-    <label className="text-black-200"> Enter Name: </label>
+    <div className="border bg-gray-200 p-5 w-[400px] rounded-md">
+    <div className=" flex flex-col gap-3">
+    <label className="text-black-100"> Enter Name: </label>
     <input type="text"
+    id="name"
     value={name}
-    onChange={(e) => setName(e.target.value)}/>
-
-<label className="text-black-200"> Enter Feedback description </label>
-    <input type="text"
-    value={feedback_desc}
-    onChange={(e)=> setFeedback_desc(e.target.value)}/>
-
-    <button className="border-red-300"
+    onChange={(e) => setName(e.target.value)}
+    className="w-[200px] p-2 rounded-md"/>
+<label className="text-black-100"> Enter Feedback description: </label>
+    {/* <input type="textarea"
+   
+    className="w-[300px] rounded-md h-[100px]"/> */}
+    <textarea name="desc" cols ={40} rows={4}
+     value={feedback_desc}
+     onChange={(e)=> setFeedback_desc(e.target.value)}
+     className="rounded-md p-2 "> </textarea>
+</div>
+<div className="flex flex-row gap-4">
+    <button className="mt-4 p-2 rounded-lg bg-teal-400"
     onClick={saveFeedback}>
         Create Feedback
     </button>
+    <Link to='/' className="mt-4 p-2 rounded-lg bg-teal-400">
+        Back to Home
+    </Link>
+    </div>
+    </div>
 </div>
 )}
 
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -2,28 +2,30 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { response } from "express";
 import { Link } from "react-router-dom";
-interface feedbackI {
-    id: string,
-    name: string,
-    feedback_desc: string
-}
-let a: Array<feedbackI> = [];
+
+let feedbackList = ""
+
 const Home = () =>{
-const [feedbacks, setFeedbacks] = useState<feedbackI[]>([]);
 const [loading, setLoading] = useState(false);
 useEffect(()=>{
     setLoading(true);
     axios
     .get('http://localhost:5000/feedback')
     .then((response)=>{
-        response.data.forEach(element=> {
-            let feedbackObj: feedbackI = {
-                id: element.id,
-                name: element.name,
-                feedback_desc: element.feedback_desc
+        if (response.data.length > 0)
+            {
+                feedbackList = response.data.map((item: { id: string, name: string, feedback_desc: string }) => {
+                    return (
+                        <div className="bg-teal-300 w-[600px] rounded-md mt-[10px] p-2" 
+                        key={item.id}> 
+                            <p>Name: {item.name}</p>
+                            <p>Feedback: {item.feedback_desc}</p>
+                        </div>
+                    )
+                })
+                console.log(feedbackList);
             }
-        });
-        setFeedbacks(a);
+        
         setLoading(false);
     })
     .catch((error)=>{
@@ -32,18 +34,18 @@ useEffect(()=>{
     })
 },[])
 
-let feedbackHtml = a.filter((feedback) => {
-    return `<div>${feedback.id}</div>`
-})
 
 return (
-<div className="p-4">
-    <button className="rounded-md bg-red-100">
-    <Link to = "/CreateFeedback">
+    
+<div className="p-5 flex flex-col ">
+    <span className="font-semibold"> FEEDBACKS </span>
+    <div className="h-[400px] w-[640px] overflow-scroll">
+    {feedbackList}
+    </div>
+    <Link to = "/CreateFeedback"
+    className="p-3 mt-4 w-[150px] rounded-md bg-teal-500">
     Create Feedback
     </Link>
-    </button>
-    
 </div>
 
 )}
